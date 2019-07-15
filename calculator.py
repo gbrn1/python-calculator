e12b1578792e089ee5feba5fb285406db97f8867
Gabriel Barone <gbarone2004@gmail.com>
2019-07-15 01:25:00 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,18 +16,25 @@ def divideFunction(a,b):
 def raiseFunction(a,b):
     return a**b
 
+def radiciationFunction(a,b):
+    return a**(1/b)
+
 showtext = ''
 text = ''
 
 root = Tk()
-root.geometry('300x300')
+root.geometry('400x400')
 root.title('Calculator')
+root.resizable(0,0)
+root.iconbitmap('calculator_icon.ico')
+
+canvas = Canvas(root, height=100, width=600, bg='#30302f')
 
 painel = Frame(root)
 painel.pack()
-txt = Label(painel, text=showtext)
+txt = Label(painel, text=showtext, fg='white', bg='#30302f')
 txt.pack()
-txt["font"] = ("Calibri", "20", "bold")
+txt["font"] = ("Arial", "32", "bold")
 
 factors = []
 sign = 0
@@ -75,6 +82,9 @@ def SetSign(Value):
         if Value == 5:
             showtext+='**'
             txt.configure(text=showtext)
+        if Value == 6:
+            showtext+='√'
+            txt.configure(text=showtext)
         text = ''
 
 def Result():
@@ -104,6 +114,8 @@ def Result():
                 clear()
         if sign == 5:
             result = raiseFunction(a,b) 
+        if sign == 6:
+            result = radiciationFunction(a,b)
         showtext = str(result)
         txt.configure(text=showtext)
         sign = 0
@@ -131,50 +143,58 @@ def clear():
     global sign
     global painel
     global factors
+    text = ''
     showtext = ''
     txt.configure(text=showtext)
     factors = []
     sign = 0
 
 # create buttons
-plus = Button(root, text='+', height='2', width='8', command=lambda:SetSign(1))
-minus = Button(root, text='-', height='2', width='8', command=lambda:SetSign(2))
-multiply = Button(root, text='x', height='2', width='8', command=lambda:SetSign(3))
-divide = Button(root, text='/', height='2', width='8', command=lambda:SetSign(4))
-potency = Button(root, text='**', height='2', width='8', command=lambda:SetSign(5))
-equal = Button(root, text='=', height='2', width='8',command=Result)
-num0 = Button(root, text='0', height='2', width='8', command=lambda:AddFactor('0'))
-num1 = Button(root, text='1', height='2', width='8', command=lambda:AddFactor('1'))
-num2 = Button(root, text='2', height='2', width='8', command=lambda:AddFactor('2'))
-num3 = Button(root, text='3', height='2', width='8', command=lambda:AddFactor('3'))
-num4 = Button(root, text='4', height='2', width='8', command=lambda:AddFactor('4'))
-num5 = Button(root, text='5', height='2', width='8', command=lambda:AddFactor('5'))
-num6 = Button(root, text='6', height='2', width='8', command=lambda:AddFactor('6'))
-num7 = Button(root, text='7', height='2', width='8', command=lambda:AddFactor('7'))
-num8 = Button(root, text='8', height='2', width='8', command=lambda:AddFactor('8'))
-num9 = Button(root, text='9', height='2', width='8', command=lambda:AddFactor('9'))
-dot = Button(root, text='.', height='2', width='8', command=lambda:AddFactor('.'))
-rmv = Button(root, text='>', height='2', width='8', command=remove)
-clr = Button(root, text='C', height='2', width='8', command=clear)
+
+
+potency = Button(root, text='**',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:SetSign(5))
+radiciation = Button(root, text='√',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:SetSign(6))
+equal = Button(root, text='=',font='arial, 12',fg='#314961', height='3', width='11',bg='#ffbe54',bd=1,command=Result)
+num0 = Button(root, text='0',font='arial, 12',fg='#314961', height='3', width='21',bd=1, command=lambda:AddFactor('0'))
+num1 = Button(root, text='1',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('1'))
+num2 = Button(root, text='2',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('2'))
+num3 = Button(root, text='3',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('3'))
+num4 = Button(root, text='4',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('4'))
+num5 = Button(root, text='5',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('5'))
+num6 = Button(root, text='6',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('6'))
+num7 = Button(root, text='7',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('7'))
+num8 = Button(root, text='8',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('8'))
+num9 = Button(root, text='9',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('9'))
+dot = Button(root, text=',',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=lambda:AddFactor('.'))
+rmv = Button(root, text='>',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=remove)
+clr = Button(root, text='C',font='arial, 12',fg='#314961', height='3', width='10',bd=1, command=clear)
+plus = Button(root, text='+',font='arial, 12',fg='#314961',height='3', width='11',bg='#ffbe54',bd=0.5, command=lambda:SetSign(1))
+minus = Button(root, text='-',font='arial, 12',fg='#314961', height='3', width='11',bg='#ffbe54',bd=0.5, command=lambda:SetSign(2))
+multiply = Button(root, text='x',font='arial, 12',fg='#314961', height='3', width='11',bg='#ffbe54',bd=0.5, command=lambda:SetSign(3))
+divide = Button(root, text='/',font='arial, 12',fg='#314961', height='3', width='11',bg='#ffbe54',bd=0.5, command=lambda:SetSign(4))
 # place buttons
-equal.place(x=165,y=250) 
-plus.place(x=230,y=210)
-minus.place(x=230,y=170)
-multiply.place(x=230,y=130)
-divide.place(x=230,y=250)
-dot.place(x=35,y=250)
-num0.place(x=100,y=250)
-num1.place(x=35,y=210)
-num2.place(x=100,y=210)
-num3.place(x=165,y=210)
-num4.place(x=35,y=170)
-num5.place(x=100,y=170)
-num6.place(x=165,y=170)
-num7.place(x=35,y=130)
-num8.place(x=100,y=130)
-num9.place(x=165,y=130)
-clr.place(x=35,y=90)
-rmv.place(x=100,y=90)
-potency.place(x=165,y=90)
+equal.place(x=294,y=334) 
+plus.place(x=294,y=268)
+minus.place(x=294,y=202)
+multiply.place(x=294,y=136)
+divide.place(x=294,y=70)
+dot.place(x=196,y=334)
+num0.place(x=0,y=334)
+num1.place(x=0,y=268)
+num2.place(x=98,y=268)
+num3.place(x=196,y=268)
+num4.place(x=0,y=202)
+num5.place(x=98,y=202)
+num6.place(x=196,y=202)
+num7.place(x=0,y=136)
+num8.place(x=98,y=136)
+num9.place(x=196,y=136)
+clr.place(x=0,y=70)
+#rmv.place(x=100,y=90)
+potency.place(x=196,y=70)
+radiciation.place(x=98,y=70)
+
+canvas.place(x=-10,y=-10)
+
 # main loop
 root.mainloop()
